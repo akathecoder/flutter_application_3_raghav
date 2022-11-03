a5d0e5c7ce4a825c773efad7595cb0d8158f7bdb
Sparsh Agarwal <sparshaga25@gmail.com>
2022-11-03 15:15:18 +0530

    added carousol

--- lib/my_homepage.dart
+++ lib/my_homepage.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
@@ -39,6 +40,16 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    List images = [
+      "https://i.picsum.photos/id/743/1080/1920.jpg?hmac=RvGxnYrwqtE3kqI8ICz34JUv4pxO42BKzuZGjYFj5ss",
+      "https://i.picsum.photos/id/315/1080/1920.jpg?hmac=jJwKgQg9wIwh80H52ANGgK53I0dEQs74BCx8-ZjnLDI",
+      "https://i.picsum.photos/id/576/1080/1920.jpg?hmac=TwGgbCLuXkT7Uu41W5AVcG5jpiv6kBebu6Qv4Ktbu6c",
+      "https://i.picsum.photos/id/514/1080/1920.jpg?hmac=W9HSpDWz0ISVRsHol2miHGJINKCOMM-M0Zmr4-B4Lfo",
+      "https://i.picsum.photos/id/415/1080/1920.jpg?hmac=Ni4XBJTexMi1FEq5MrGqNsGksg10EzoNrpMqInK-VeQ",
+      "https://i.picsum.photos/id/618/1080/1920.jpg?hmac=jECgxQudPKJiRA7jFv64jxuzL4O_KbP9EO2Yej-QT58",
+      "https://i.picsum.photos/id/859/1080/1920.jpg?hmac=7Qy_WZovzz76LdKCdANtz_fj3w_rgUxwSekulXRyy_I",
+    ];
+
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
@@ -48,6 +59,27 @@ class _MyHomePageState extends State<MyHomePage> {
           statusBarColor: Colors.blue,
           statusBarIconBrightness: Brightness.dark, // For Android (dark icons)
           statusBarBrightness: Brightness.light, // For iOS (dark icons)
+        ),
+      ),
+      body: SafeArea(
+        child: Center(
+          child: CarouselSlider.builder(
+            itemCount: images.length,
+            itemBuilder:
+                (BuildContext context, int itemIndex, int pageViewIndex) =>
+                    Center(
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                margin: const EdgeInsets.symmetric(horizontal: 5.0),
+                decoration: const BoxDecoration(color: Colors.amber),
+                child: Image.network(
+                  images[itemIndex],
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            options: CarouselOptions(height: 400.0),
+          ),
         ),
       ),
       bottomNavigationBar: BottomNavigationBar(
